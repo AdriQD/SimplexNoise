653d19744979fe604e37a103495d53c44f04723a
AdriQD <46886493+AdriQD@users.noreply.github.com>
2024-11-22 00:17:08 +0100

    killed xrange

--- noise_image.py
+++ noise_image.py
@@ -7,13 +7,13 @@ noise_scale = 700.0 # Turns up the contrast
 sn = SimplexNoise(num_octaves=7, persistence=0.1, dimensions=2, noise_scale=noise_scale)
 data = []
 
-for i in xrange(size):
+for i in range(size):
     data.append([])
-    for j in xrange(size):
+    for j in range(size):
         noise = normalize(sn.noise(i, j))
         data[i].append(noise * 255.0)
 
 # Cast to numpy array so we can save 
 data = np.array(data).astype(np.uint8)
 img = Image.fromarray(data, mode='L')
-img.save('./noise_example.png')
+img.save('./noise_exampleB.png')
